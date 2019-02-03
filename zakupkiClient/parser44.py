70b460b221292215325dd9dfa7542da1b6df4faf
macbook <isaevnextdoor@gmail.com>
2019-02-03 23:35:40 +0300

    simplify

--- zakupkiClient/parser44.py
+++
@@ -1,9 +0,0 @@
-from .parserinterface import ParserInterface
-from .util import _dump_JSON_data, _checkDirectory_if_not_create
-from .webutils import *
-
-
-class Parser44(ParserInterface):
-    def __init__(self, stub):
-        self.__stub = stub
-
